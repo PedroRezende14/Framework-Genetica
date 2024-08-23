eb41ba1953022ecbb8486397158b06aa59dd2f95
Rezende <128604858+PedroRezende14@users.noreply.github.com>
2024-08-23 10:59:00 -0300

    doc duplicado

--- framework/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Genetica","l":"Codon"},{"p":"Genetica","l":"FastaBaixar"},{"p":"Genetica","l":"FastaComparar"},{"p":"Genetica","l":"GeneticaFacade"}];updateSearchResults();
